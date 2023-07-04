6df3f649316fa125212d2ad21e3bb6b688566029
Ricardo Sánchez <rsanchezsk@gmail.com>
2023-07-04 16:35:44 -0600

    series-stories page first load

--- lib/view/character_details/series_page/series_page.dart
+++ lib/view/character_details/series_page/series_page.dart
@@ -1,16 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SeriesPage extends StatefulWidget {
+import 'package:marvel_test/view/wigdets/empty_data.dart';
+import 'package:marvel_test/view/wigdets/custom_loader.dart';
+import 'package:marvel_test/view/character_details/series_page/widgets/serie_item.dart';
+import 'package:marvel_test/view_model/character_detail/serie_vm/serie_view_model.dart';
+
+final _serieProvider = Provider.autoDispose(
+  (ref) => ref.watch(seriesProvider),
+);
+
+class SeriesPage extends ConsumerStatefulWidget {
   final int characterId;
+
   const SeriesPage({super.key, required this.characterId});
 
   @override
-  State<SeriesPage> createState() => _SeriesPageState();
+  ConsumerState<SeriesPage> createState() => _SeriesPageState();
 }
 
-class _SeriesPageState extends State<SeriesPage> {
+class _SeriesPageState extends ConsumerState<SeriesPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(seriesProvider.notifier).getSerieList(
+            charId: widget.characterId,
+            offset: 0,
+            refresh: true,
+          );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final serieList = ref.watch(_serieProvider);
+
+    return RefreshIndicator(
+      onRefresh: () => _loadData(0, refresh: true),
+      child: serieList.isLoading ||
+              (!serieList.isLoading &&
+                  serieList.failure == null &&
+                  serieList.response == null)
+          ? const CustomLoader(size: SpinnerSize.normal)
+          : serieList.failure != null || serieList.response!.isEmpty
+              ? const EmptyData()
+              : GridView.builder(
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  padding: EdgeInsets.symmetric(horizontal: 5.w),
+                  itemCount: serieList.response!.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemBuilder: (_, index) {
+                    if (index < serieList.response!.length) {
+                      var serie = serieList.response![index];
+                      return SerieItem(serie: serie);
+                    } else {
+                      return const CustomLoader(size: SpinnerSize.small);
+                    }
+                  },
+                ),
+    );
+  }
+
+  Future<void> _loadData(int offset, {bool refresh = false}) async {
+    ref.read(seriesProvider.notifier).getSerieList(
+          charId: widget.characterId,
+          offset: offset,
+          refresh: refresh,
+        );
   }
 }
--- lib/view/character_details/stories_page/stories_page.dart
+++ lib/view/character_details/stories_page/stories_page.dart
@@ -1,16 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class StoriesPage extends StatefulWidget {
+import 'package:marvel_test/view/wigdets/empty_data.dart';
+import 'package:marvel_test/view/wigdets/custom_loader.dart';
+import 'package:marvel_test/view/character_details/stories_page/widgets/story_item.dart';
+import 'package:marvel_test/view_model/character_detail/story_vm/story_view_model.dart';
+
+final _storyProvider = Provider.autoDispose(
+  (ref) => ref.watch(storiesProvider),
+);
+
+class StoriesPage extends ConsumerStatefulWidget {
   final int characterId;
+
   const StoriesPage({super.key, required this.characterId});
 
   @override
-  State<StoriesPage> createState() => _StoriesPageState();
+  ConsumerState<StoriesPage> createState() => _StoriesPageState();
 }
 
-class _StoriesPageState extends State<StoriesPage> {
+class _StoriesPageState extends ConsumerState<StoriesPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(storiesProvider.notifier).getStoryList(
+            charId: widget.characterId,
+            offset: 0,
+            refresh: true,
+          );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final storyList = ref.watch(_storyProvider);
+
+    return RefreshIndicator(
+      onRefresh: () => _loadData(0, refresh: true),
+      child: storyList.isLoading ||
+              (!storyList.isLoading &&
+                  storyList.failure == null &&
+                  storyList.response == null)
+          ? const CustomLoader(size: SpinnerSize.normal)
+          : storyList.failure != null || storyList.response!.isEmpty
+              ? const EmptyData()
+              : GridView.builder(
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  padding: EdgeInsets.symmetric(horizontal: 5.w),
+                  itemCount: storyList.response!.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemBuilder: (_, index) {
+                    if (index < storyList.response!.length) {
+                      var story = storyList.response![index];
+                      return StoryItem(story: story);
+                    } else {
+                      return const CustomLoader(size: SpinnerSize.small);
+                    }
+                  },
+                ),
+    );
+  }
+
+  Future<void> _loadData(int offset, {bool refresh = false}) async {
+    ref.read(storiesProvider.notifier).getStoryList(
+          charId: widget.characterId,
+          offset: offset,
+          refresh: refresh,
+        );
   }
 }
