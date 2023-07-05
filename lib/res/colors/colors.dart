de8d9a68c00f68f922375b860536ba8c5fd7a533
Ricardo Sánchez <rsanchezsk@gmail.com>
2023-07-04 18:45:45 -0600

    unused code cleaned

--- lib/res/colors/colors.dart
+++ lib/res/colors/colors.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color marvelDarkGrey = Color(0xFF091B26);
 const Color marvelBlueGrey = Color(0xFF243542);
 const Color marvelGrey = Color(0xFF6C898C);
 const Color marvelBrown = Color(0xFF260F01);
